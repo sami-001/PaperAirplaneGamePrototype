5ec7d7442d36cfe4c3817410a440eb3ca32ce016
sami-001 <samiaboualfadl@gmail.com>
2024-10-21 15:32:38 +0300

    Restructure

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 #include <SDL2/SDL.h>
 
+int processEvents(SDL_Window *window);
+void doRender(SDL_Renderer *renderer);
+int doPhysics();
 
 int main(int argc, char *argv[]) {
 
@@ -20,28 +23,38 @@ int main(int argc, char *argv[]) {
         printf("Could not create renderer. Error: %s", SDL_GetError()); 
         return -1;
     }
-    
+
     int running = 1;
-
-    SDL_Event e;
     while (running) {
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 225);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderPresent(renderer);
-
-        while (SDL_PollEvent(&e)) {
-            switch (e.type) 
-                case SDL_QUIT:
-                    running = 0;
-                break;
-        }
+        running = processEvents(window);
+        doRender(renderer);   
     }
+
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
 
     SDL_Quit();
     return 0;
+}
+
+void doRender(SDL_Renderer *renderer) {
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 225);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
+int processEvents(SDL_Window *window) {
+    int running = 1;
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        switch (e.type) 
+        case SDL_QUIT:
+            running = 0;
+            break;
+    }
+    return running;
+
 }
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H 
+
+#include <stdio.h>
+#include <SDL2/SDL.h> 
+
+typedef struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+} Main;
+
+#endif
